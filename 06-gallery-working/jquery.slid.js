191ae1cd6cbf4b6f122c024240a5e6f12dc0f36b
mariiapitsenko <32640564+mariiapitsenko@users.noreply.github.com>
2017-10-25 17:27:37 +0300

    33

--- 06-gallery-working/jquery.slid.js
+++ 06-gallery-working/jquery.slid.js
@@ -1,44 +1,54 @@
-(function($){
-    $.fn.myGallery = function(options){
-        var settings = $.extend({
-            $slider: 'div.image-item',
-            $prev : "a.slide-prev",
-            $next: 'a.slide-next' 
-        }, options);
-        return this.each(function(){
-            var currentslide = 0;
-            
-            var $slider = $(this).find('.image-item'),
-            $parent = $(this),
-            $next = $parent.find(".slide-next"),
-            $prev = $parent.find(".slide-prev");
+(function($) {
+  $.fn.myGallery = function(options) {
+    var settings = $.extend(
+      {
+        $slider: "div.image-item",
+        $prev: "a.slide-prev",
+        $next: "a.slide-next"
+      },
+      options
+    );
+    return this.each(function() {
+      var currentslide = 0;
 
-           this.slide = function () {
-                var item = $slider.eq(currentslide);
-                $slider.hide();
-                item.css("display", "inline-block");
-            }
-            slide();
-            this.next = function () {
-            currentslide += 1;
-            
-            if(currentslide > $slider.length -1)  {
-                   currentslide = 0;
-            }
-               slide();
-            }
-            
-            this.prev = function (){
-            currentslide -= 1;
-            
-            if(currentslide < 0)  {                        
-                currentslide = $slider.length -1;
-                }
-                   slide();               
-                }
-            
-            $next.on("click", next);
-            $prev.on("click", prev);
-        });
-    };
+      var $slider = $(this).find(".image-item"),
+        $parent = $(this),
+        $next = $parent.find(".slide-next"),
+        $prev = $parent.find(".slide-prev");
+
+      slide = function() {
+        var item = $slider.eq(currentslide);
+        $slider.hide();
+        item.css("display", "inline-block");
+        item.animate({ "opacity": "1" }, 1000);
+      };
+      slide();
+      next = function() {
+        currentslide += 1;
+        console.log(currentslide);
+        var prevS  = currentslide-1;
+        console.log(prevS);
+        // prevS.animate({'opacity' : '0'}, 1000);
+
+        // currentslide.animate({'opacity' : '1'}, 1000);
+
+        if (currentslide > $slider.length - 1) {
+          currentslide = 0;
+        }
+        slide();
+      };
+
+      prev = function() {
+        currentslide -= 1;
+
+        if (currentslide < 0) {
+          currentslide = $slider.length - 1;
+        }
+        slide();
+      };
+
+      $next.on("click", next);
+      $prev.on("click", prev);
+    });
+  };
 })(jQuery);
