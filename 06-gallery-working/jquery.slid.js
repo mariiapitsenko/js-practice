8d751c5411c8daa48312ec6b761be51da5b296e1
mariiapitsenko <32640564+mariiapitsenko@users.noreply.github.com>
2017-10-25 18:38:10 +0300

    done with animate(opacity)

--- 06-gallery-working/jquery.slid.js
+++ 06-gallery-working/jquery.slid.js
@@ -10,26 +10,23 @@
     );
     return this.each(function() {
       var currentslide = 0;
-
+      var prevSlide;
       var $slider = $(this).find(".image-item"),
         $parent = $(this),
         $next = $parent.find(".slide-next"),
         $prev = $parent.find(".slide-prev");
+      $slider.eq(currentslide).css("opacity", "1");
 
       slide = function() {
         var item = $slider.eq(currentslide);
-        $slider.hide();
-        item.css("display", "inline-block");
-        item.animate({ "opacity": "1"}, 1000 );
+        $slider.eq(prevSlide).animate({ opacity: 0 });
+        $slider.eq(currentslide).animate({ opacity: 1 });
       };
-      slide();
+
       next = function() {
+        prevSlide = currentslide;
         currentslide += 1;
-        // prevS.animate({'opacity' : '0'}, 1000);
         
-        // item.animate({'opacity' : '1'}, 1000);
-
-
         if (currentslide > $slider.length - 1) {
           currentslide = 0;
         }
@@ -37,8 +34,9 @@
       };
 
       prev = function() {
+        prevSlide = currentslide;
         currentslide -= 1;
-
+        
         if (currentslide < 0) {
           currentslide = $slider.length - 1;
         }
